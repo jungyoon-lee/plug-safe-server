d72dd9ca5f1220cc2c1dfa089eef22ea21013627
agurimon <agurimon18@gmail.com>
2019-08-30 01:08:43 +0900

    basic frame

---
+++ app/static/js/layout.js
@@ -0,0 +1,15 @@
+require(['/static/config.js'], function () {
+  require(['jquery', 'semantic'], function ($, Semantic) {
+
+    var ua = window.navigator.userAgent;
+    var msie = ua.indexOf("MSIE ");
+
+    if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+      console.log("MS IE Detected :(");
+    }
+    else {
+      $('#content').addClass('sticky-content');
+      $('#body').addClass('sticky-body');
+    }
+  });
+});
